9d5f03004327d0170dd914eb2b75820ebc2dac02
Morgan-Harvey <146832750+Morgan-Harvey@users.noreply.github.com>
2024-02-27 11:24:46 +0000

    nearly_done

--- test.sh
+++ test.sh
@@ -1,30 +1,70 @@
 #!/bin/bas
 
-chmod +x <test.sh>
+chmod +x test.sh
 
-# count the number of tests completed
-((all_counter++))
+# removed the permissions of the file to test for bad permissions
+chmod -r data/bad_perms.txt
 
-
-echo -e "~~Symbol Precense~~\n"
-echo -n "no player present"
-
-./program 
-if grep -q "Player is not present";
+# testing errors in arguments
+echo -e "~~ Argument Tests ~~"
+echo -n "Testing no arguments - "
+./program > tmp
+if grep -q "Usage: studentData <filename>" tmp;
 then
-    echo "\e[32mPASS\e[0mm"
-    ((pass_counter++))
+    echo "PASS"
 else
-    echo "\e[31mFAIL\e[0m"
+    echo "FAIL"
+fi
+
+echo -n "Testing 2 arguments - "
+./Program y z > tmp
+if grep -q "Usage: Program <Maze file>" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+ech0
+
+
+echo -e "\n~~ File Handling~~"
+
+echo -n "Testing bad mapname - "
+./studentData rge_1 x 5.txt > tmp
+if grep -q "Error: unknown map" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
 fi
 
 
+echo -n "Testing bad permissions - "
+./studentData maps/invalid/bad_perms.txt > tmp
+if grep -q "Error: Bad permissions" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
 
-echo -n "no player present"
-if grep -q "Start is not present";
+echo -n "Testing file loading - "
+./studentData maps/valid/reg_5x5.txt.txt > tmp
+if grep -q "Error: Bad permissions" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+echo -e "~~Symbol Precense~~\n"
+
+./program maps/invalid/MissingStart.txt > tmp
+echo -n "no start present"
+if grep -q "Start is missing" tmp;
 then
     echo "\e[32mPASS\e[0mm"
-    ((pass_counter++))
 else
     echo "\e[31mFAIL\e[0m"
 fi
@@ -32,6 +72,7 @@ fi
 
 
 echo -n "no End present"
+./program maps/invalid/MissingEnd.txt > tmp
 if grep -q "End is not present";
 then
     echo "\e[32mPASS\e[0mm"
@@ -41,6 +82,7 @@ else
 fi
 
 echo -n "Multiple start present"
+./program maps/invalid/MultipleStart.txt > tmp
 if grep -q "More than one start is present";
 then
     echo "\e[32mPASS\e[0mm"
@@ -51,6 +93,7 @@ fi
 
 
 echo -n "Multiple End present"
+./program maps/invalid/MultipleEnd.txt > tmp
 if grep -q "more than one end is present";
 then
     echo "\e[32mPASS\e[0mm"
@@ -70,7 +113,8 @@ fi
 echo -e "~~player input~~\n"
 echo -n "Testing unknown input"
 
-if grep -q "Unknown Input detected";
+./program maps/valid/dierection_texting.txt < inputs/unknown_input.txt > tmp
+if grep -q "Unknown Input detected" tmp;
 then
     echo "\e[32mPASS\e[0mm"
     ((pass_counter++))
@@ -81,7 +125,7 @@ fi
 
 echo -n "Testing W input"
 
-./program 
+./program maps/valid/dierection_texting.txt < inputs/Move_up.txt.txt > tmp
 if grep -q "You pressed W";
 then
     echo "\e[32mPASS\e[0mm"
@@ -91,7 +135,7 @@ else
 fi
 
 echo -n "Testing A input"
-
+./program maps/valid/dierection_texting.txt < inputs/Move_left.txt.txt > tmp
 if grep -q "You pressed A";
 then
     echo "\e[32mPASS\e[0mm"
@@ -101,7 +145,7 @@ else
 fi
 
 echo -n "Testing S input"
-./program maps/valid/wall_testing.txt <inputs/Move down.txt> tmp
+./program maps/valid/wall_testing.txt < inputs/Move_down.txt > tmp
 if grep -q "You pressed S";
 then
     echo "\e[32mPASS\e[0mm"
@@ -111,7 +155,7 @@ else
 fi
 
 echo -n "Testing D input"
-
+./program maps/valid/dierection_texting.txt < inputs/Move_right.txt > tmp
 if grep -q "You have moved right";
 then
     echo "\e[32mPASS\e[0mm"
@@ -152,13 +196,12 @@ fi
 
 
 echo -n "~~Goal~~\n"
-echo -n "players testing"
+echo -n "reaching the goal"
+./programs 
 
 echo -n "~~players testing~~\n"
 echo -n "~~players testing"
 
 
-echo -n "~~ tests complete~~ "
-echo -n "Total tests: $all_counter"
-echo -n "Passed tests: $pass_counter"
-echo -n "Percentage passed: $(echo "scale=2; $pass_counter*100 / $all_counter"
+
+chmod +r data/bad_perms.csv
